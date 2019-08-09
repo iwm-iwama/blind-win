db689a7ccfffce1c1f2f85af5ec355f5ec030aa2
iwm-iwama <52396966+iwm-iwama@users.noreply.github.com>
2019-08-09 11:39:05 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace iwm_blind
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.Cms1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.Cms1_最大化_解除 = new System.Windows.Forms.ToolStripMenuItem();
+			this.Cms1_L1 = new System.Windows.Forms.ToolStripSeparator();
+			this.Cms1_背景色 = new System.Windows.Forms.ToolStripMenuItem();
+			this.Cms1_L2 = new System.Windows.Forms.ToolStripSeparator();
+			this.Cms1_閉じる = new System.Windows.Forms.ToolStripMenuItem();
+			this.ToolTip1 = new System.Windows.Forms.ToolTip(this.components);
+			this.ColorDialog1 = new System.Windows.Forms.ColorDialog();
+			this.Cms1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// Cms1
+			// 
+			this.Cms1.BackColor = System.Drawing.Color.White;
+			this.Cms1.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.Cms1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Cms1_最大化_解除,
+            this.Cms1_L1,
+            this.Cms1_背景色,
+            this.Cms1_L2,
+            this.Cms1_閉じる});
+			this.Cms1.Name = "contextMenuStrip1";
+			this.Cms1.Size = new System.Drawing.Size(181, 104);
+			// 
+			// Cms1_最大化_解除
+			// 
+			this.Cms1_最大化_解除.BackColor = System.Drawing.Color.White;
+			this.Cms1_最大化_解除.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.Cms1_最大化_解除.Name = "Cms1_最大化_解除";
+			this.Cms1_最大化_解除.Size = new System.Drawing.Size(180, 22);
+			this.Cms1_最大化_解除.Text = "最大化／解除";
+			this.Cms1_最大化_解除.Click += new System.EventHandler(this.Cms1_最大化_解除_Click);
+			// 
+			// Cms1_L1
+			// 
+			this.Cms1_L1.Name = "Cms1_L1";
+			this.Cms1_L1.Size = new System.Drawing.Size(177, 6);
+			// 
+			// Cms1_背景色
+			// 
+			this.Cms1_背景色.Name = "Cms1_背景色";
+			this.Cms1_背景色.Size = new System.Drawing.Size(180, 22);
+			this.Cms1_背景色.Text = "■背景色";
+			this.Cms1_背景色.Click += new System.EventHandler(this.Cms1_背景色_Click);
+			// 
+			// Cms1_L2
+			// 
+			this.Cms1_L2.Name = "Cms1_L2";
+			this.Cms1_L2.Size = new System.Drawing.Size(177, 6);
+			// 
+			// Cms1_閉じる
+			// 
+			this.Cms1_閉じる.BackColor = System.Drawing.Color.White;
+			this.Cms1_閉じる.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.Cms1_閉じる.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.Cms1_閉じる.Name = "Cms1_閉じる";
+			this.Cms1_閉じる.Size = new System.Drawing.Size(180, 22);
+			this.Cms1_閉じる.Text = "閉じる";
+			this.Cms1_閉じる.Click += new System.EventHandler(this.Cms1_閉じる_Click);
+			// 
+			// ToolTip1
+			// 
+			this.ToolTip1.AutomaticDelay = 0;
+			this.ToolTip1.AutoPopDelay = 2500;
+			this.ToolTip1.InitialDelay = 5000;
+			this.ToolTip1.ReshowDelay = 5000;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(304, 304);
+			this.ContextMenuStrip = this.Cms1;
+			this.ControlBox = false;
+			this.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+			this.Name = "Form1";
+			this.RightToLeftLayout = true;
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.TopMost = true;
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+			this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+			this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+			this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+			this.Cms1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ContextMenuStrip Cms1;
+		private System.Windows.Forms.ToolStripSeparator Cms1_L1;
+		private System.Windows.Forms.ToolStripMenuItem Cms1_最大化_解除;
+		private System.Windows.Forms.ToolStripMenuItem Cms1_閉じる;
+		private System.Windows.Forms.ToolTip ToolTip1;
+		private System.Windows.Forms.ToolStripMenuItem Cms1_背景色;
+		private System.Windows.Forms.ToolStripSeparator Cms1_L2;
+		private System.Windows.Forms.ColorDialog ColorDialog1;
+	}
+}
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iwm_blind
+{
+	public partial class Form1 : Form
+	{
+		///private const string Ver = "iwm20190416";
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			// MouseWhell イベント追加
+			MouseWheel += new MouseEventHandler(Form_MouseWheel);
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			SubBgBlind();
+		}
+
+		private void Form1_MouseEnter(object sender, EventArgs e)
+		{
+			ToolTip1.SetToolTip(
+				this,
+				"[マウスホイール] 暗さを調整\n" +
+				"[ダブルクリック] 巻き上げ／下げ\n" +
+				"[右クリック] 閉じる"
+			);
+		}
+
+		private void Form_MouseWheel(object sender, MouseEventArgs e)
+		{
+			// 上回転 +120
+			if (e.Delta > 0)
+			{
+				SubBgBlindUp();
+			}
+			// 下回転 -120
+			else if (e.Delta < 0)
+			{
+				SubBgBlindDown();
+			}
+
+			ToolTip1.SetToolTip(this, (Opacity * 100) + "%");
+		}
+
+		private Point MousePoint;
+
+		private void Form1_MouseDown(object sender, MouseEventArgs e)
+		{
+			if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+			{
+				MousePoint = new Point(e.X, e.Y);
+			}
+		}
+
+		private void Form1_MouseMove(object sender, MouseEventArgs e)
+		{
+			if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+			{
+				Left += e.X - MousePoint.X;
+				Top += e.Y - MousePoint.Y;
+			}
+		}
+
+		private const int GblHeighttMin = 50;
+		private int GblHeighttMax = 0;
+
+		private void Form1_DoubleClick(object sender, EventArgs e)
+		{
+			if (WindowState == FormWindowState.Maximized)
+			{
+				WindowState = FormWindowState.Normal;
+				return;
+			}
+
+			if (Height > GblHeighttMin)
+			{
+				GblHeighttMax = Height;
+				Height = GblHeighttMin;
+			}
+			else
+			{
+				Height = GblHeighttMax;
+			}
+		}
+
+		private void Cms1_背景色_Click(object sender, EventArgs e)
+		{
+			ColorDialog CD = new ColorDialog
+			{
+				Color = BackColor,
+				AllowFullOpen = true,
+				SolidColorOnly = true,
+				CustomColors = new int[]
+				{
+					0x0000ff, 0x0000cc, 0x000099, 0x000066,
+					0x00ff00, 0x00cc00, 0x009900, 0x006600,
+					0xff0000, 0xcc0000, 0x990000, 0x660000,
+					0x00ffff, 0xffff00, 0xff00ff, 0x000000
+				}
+			};
+
+			if (CD.ShowDialog() == DialogResult.OK)
+			{
+				BackColor = CD.Color;
+			}
+		}
+
+		private void Cms1_最大化_解除_Click(object sender, EventArgs e)
+		{
+			WindowState = WindowState == FormWindowState.Maximized ? FormWindowState.Normal : FormWindowState.Maximized;
+		}
+
+		private void Cms1_閉じる_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		//-------
+		// 関数
+		//-------
+		private void SubBgBlind(
+			double level = 0.6
+		)
+		{
+			if (level > 1.0)
+			{
+				level = 0.1;
+			}
+			else if (level < 0.1)
+			{
+				level = 1.0;
+			}
+
+			Opacity = level;
+		}
+
+		private void SubBgBlindUp()
+		{
+			SubBgBlind(Opacity + 0.1);
+		}
+
+		private void SubBgBlindDown()
+		{
+			SubBgBlind(Opacity - 0.1);
+		}
+	}
+}
